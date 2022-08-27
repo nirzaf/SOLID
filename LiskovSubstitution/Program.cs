82e15be572fa1421e9fd1fd1e01c8bdbb4f8866a
MFM_Fazrin <mmfazrin@phcc.gov.qa>
2022-08-27 15:04:39 +0300

    Initial commit

---
+++ LiskovSubstitution/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Area of the rectangle ..");
+
+Rectangle rec = new(2, 3);
+Console.WriteLine($"Area of the rectangle : {Area(rec)}");
+
+Rectangle sq = new Square();
+sq.Width = 4;
+
+Console.WriteLine($"Area of the Square : {Area(sq)}");
+
+
+Console.ReadLine();
+
+
+int Area(Rectangle r)=> r.Width * r.Height;
+
+public class Rectangle
+{
+    public virtual int Width { get; set; }
+    public virtual int Height { get; set; }
+
+    public Rectangle()
+    {
+        
+    }
+
+    public Rectangle(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+}
+
+public class Square : Rectangle
+{
+    public override int Width
+    {
+        set => base.Width = base.Height = value;
+    }
+
+    public override int Height
+    {
+        set => base.Width = base.Height = value;
+    }
+}
---
+++ SingleResponsibility/Program.cs
@@ -0,0 +1,79 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+
+public class Employee : IEmployee
+{
+    public int Id { get; set;}
+    public string Name { get; set; }
+    
+    public void AddEmployee(Employee employee)
+    {
+        Console.WriteLine("Adding employee");
+    }
+
+    public void RemoveEmployee(Employee employee)
+    {
+        Console.WriteLine("Removing employee");
+    }
+
+    public void CalculateSalary(Employee employee)
+    {
+        Console.WriteLine("Calculating salary");
+    }
+
+    public void AssignAsset(Employee employee)
+    {
+        Console.WriteLine("Assigning asset");
+    }
+}
+
+
+public class Salary
+{
+    public double BasicSalary { get; set; }
+    public double WorkedHours { get; set; }
+    
+    public virtual double CalculateSalary()
+    {
+        return BasicSalary * WorkedHours;
+    }
+}
+
+public class NewSalary : Salary
+{
+    IEmployee emp = new Employee();
+    public override double CalculateSalary()
+    {
+        //emp.AssignAsset();
+        return base.CalculateSalary() * 1.2;
+    }
+}
+
+public interface  IEmployeeCRUD
+{
+    void AddEmployee(Employee employee);
+    void RemoveEmployee(Employee employee);
+}
+
+
+public interface IEmployee : IEmployeeCRUD
+{
+    void CalculateSalary(Employee employee);
+    void AssignAsset(Employee employee);
+}
+
+public class NewEmployee : IEmployeeCRUD
+{
+    public void AddEmployee(Employee employee)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveEmployee(Employee employee)
+    {
+        throw new NotImplementedException();
+    }
+}
+
